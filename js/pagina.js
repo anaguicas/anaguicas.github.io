72b2affc4d13d44dd00785112121a6bac188703e
anaguicas <angelicaaguicas@gmail.com>
2017-06-26 17:15:44 -0500

    sitio web

---
+++ js/pagina.js
@@ -0,0 +1,49 @@
+var backUrl = "http:\/\/www.wix.com\/website\/templates\/html\/portfolio-cv\/1";
+      var editUrl = "http://editor.wix.com/html/editor/web/renderer/new?metaSiteId=25696c7e-f948-43cc-a307-463d74bcc52d&siteId=a2b2ea31-fa89-4af6-839a-0152c05a239e";
+      var galleryDocIndex = "8";
+      var originAppUrl = "http:\/\/www.wix.com\/website\/templates\/html\/portfolio-cv\/1";
+      var W = W || {};
+      W.App = W.App || {};
+      W.App.CLIENT_CONFIG = {"biServerUrl":"http://frog.wix.com/spettatore_bi?src=2&openingSiteId=a2b2ea31-fa89-4af6-839a-0152c05a239e&originAppUrl=http%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fportfolio-cv%2F1&siteType=2"};
+      W.App.currentViewMode = 'desktop';
+
+      W.Globals = W.Globals || {};
+      W.Globals.lang =  "es";
+      var analytics = {"staging":false,"code":"UA-2117194-29","domain":"wix.com"};
+      var openAsTab = true;
+
+    //Storing dynamic text for responsive design
+    var headerTxt = {};
+    headerTxt.editButtonSmall = 'Editar';
+    headerTxt.mainHeaderSmall = 'Crea tu increíble página web';
+
+
+
+
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push(
+            {'gtm.start': new Date().getTime(),event:'gtm.js'}
+            );var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-MDD5C4');
+
+
+
+      (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    /*Google+ */
+    (function () {
+        var po = document.createElement('script');
+        po.type = 'text/javascript';
+        po.async = true;
+        po.src = 'https://apis.google.com/js/plusone.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(po, s);
+    })();
